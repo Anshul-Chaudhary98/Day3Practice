647a64c12ddc8036734a5e3297c4bf5a7a9efdd7
Anshul-Chaudhary98 <101334052+Anshul-Chaudhary98@users.noreply.github.com>
2022-07-04 20:34:43 +0530

    Add files via upload

---
+++ SortElementsInAscOrder.java
@@ -0,0 +1,34 @@
+package Day3Assignments;
+
+import java.util.Scanner;
+
+public class SortElementsInAscOrder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int temp = 0;
+		System.out.print("Enter the ArraySize: ");
+		Scanner sc = new Scanner(System.in);
+		int arrSize = sc.nextInt();
+		 int [] arr = new int [arrSize]; 
+	     System.out.println("Elements Of An Array are: ");  
+	     for (int i = 0; i < arrSize; i++) {  
+	         arr[i] = sc.nextInt(); 
+	     }
+		 for (int i = 0; i < arrSize; i++) {     
+	           for (int j = i+1; j < arr.length; j++) {     
+	              if(arr[i] > arr[j]) {    
+	                  temp = arr[i];    
+	                  arr[i] = arr[j];    
+	                  arr[j] = temp;    
+	              }     
+	           }     
+	     }   
+		 System.out.println("Elements of an array sorted in ascending order: ");    
+	     for (int i = 0; i < arrSize; i++) {     
+	    	 System.out.print(arr[i] + " ");    
+	     }    
+	}
+
+}
